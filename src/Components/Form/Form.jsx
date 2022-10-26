65d60e4edb06711a3e00bd61bda17939d1f15173
stutisk <48529714+stutisk@users.noreply.github.com>
2022-10-26 19:56:17 +0530

    feat:worked on responsiveness

--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -4,9 +4,12 @@ import { Signin } from "../Button/Signin";
 import { AiOutlineEye } from "react-icons/ai";
 const Form = () => {
   return (
-    <section className={` ${styles.page}  `}>
+    <section className={` ${styles.page} flex  flex-column`}>
       <section className={` flex p2 flex-column gap3 `}>
-        <Button />
+        <section>
+          {" "}
+          <Button />
+        </section>
         <section className="grid justify-content align-content grid-gapR ">
           <img
             src="https://res.cloudinary.com/dvbw8xujd/image/upload/v1666701300/opypjpsweaheb0fzbyt4.jpg"
@@ -22,59 +25,59 @@ const Form = () => {
             <section className="heading2">Andrew@polka.io</section>
           </section>
         </section>
-        <form className="flex flex-column gap ">
-          <label for="input" className="heading1  fontWeight1">
-            Password
-          </label>
+        <section className="flex flex-column  gap">
+          {" "}
           <section>
-            <input
-              className="input p-px"
-              type="password"
-              placeholder="andrew@polka"
-            />{" "}
-            <AiOutlineEye size={25}className= {` ${styles.icons} p3 pointer`}/>
+            <label for="input" className="heading1  fontWeight1">
+              Password
+            </label>
           </section>
-         
-        </form>
-        <section className="grid justify-content align-content  ">
-            <section className="flex flex-column ">
-              <section className={`  heading1 text-center fontWeight1`}>
-                Forgot password?{" "}
-                <span className={` ${styles.spanText} fontWeight2  pointer`}>
-                  Reset
-                </span>
-              </section>
+          <section>
+            <input className=" p3" type="password" placeholder="andrew@polka" />
+            <AiOutlineEye size={25} className={` ${styles.icons} p3 pointer`} />
+          </section>
+        </section>
+
+        <section className="flex justify-content align-content  ">
+          <section className="flex flex-column ">
+            <section className={`  heading1 text-center fontWeight1`}>
+              Forgot password?{" "}
+              <span className={` ${styles.spanText} fontWeight2  pointer`}>
+                Reset
+              </span>
             </section>
           </section>
+        </section>
 
-        <section className="flex gap2  ">
-          <section className="heading1  fontWeight1 align-self ">Stay signed in</section>
+        <section className={` ${styles.mobile} flex gap2`}>
+          <section className="heading1  fontWeight1 align-self ">
+            Stay signed in
+          </section>
 
           <section>
             <Signin />
           </section>
         </section>
-       
       </section>
       <footer className="p2">
-          <section
-            className={` ${styles.heading} heading1 text-center fontWeight1`}
-          >
-            This site is protected by reCAPTCHA and the Google{" "}
-          </section>
-          <section
-            className={` ${styles.heading} heading1 text-center fontWeight1`}
-          >
-            {" "}
-            <span className={` ${styles.footerSpan}`}>
-              Privacy Policy
-            </span> and{" "}
-            <span className={` ${styles.footerSpan}`}>
-              Terms of Service apply
-            </span>
-            .
-          </section>
-        </footer>
+        <section
+          className={` ${styles.heading} heading1 text-center fontWeight1`}
+        >
+          This site is protected by reCAPTCHA and the Google{" "}
+        </section>
+        <section
+          className={` ${styles.heading} heading1 text-center fontWeight1`}
+        >
+          {" "}
+          <span className={` ${styles.footerSpan}`}>
+            Privacy Policy
+          </span> and{" "}
+          <span className={` ${styles.footerSpan}`}>
+            Terms of Service apply
+          </span>
+          .
+        </section>
+      </footer>
     </section>
   );
 };
--- src/Pages/Mainpage/Mainpage.jsx
+++ src/Pages/Mainpage/Mainpage.jsx
@@ -1,6 +1,6 @@
 import { Form } from "../../Components";
 const Mainpage = () => {
-  return <section className="grid justify-content align-content">
+  return <section className="grid justify-content align-content max-width">
     <Form/>
   </section>;
 };
